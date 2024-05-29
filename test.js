859abf6a11fc3596cc5a6bd3eaf4b65cc9434360
order-gstreetwearph <thriftedukay1@gmail.com>
2024-05-29 18:13:02 +0800

    Delete test.js

--- test.js
+++
@@ -1,9 +0,0 @@
-const express = require("express");
-const app = express();
-const PORT = process.env.PORT || 3030;
-
-console.log('Hello Bryan')
-
-app.listen(PORT, () => {
-  console.log(`server started on port ${PORT}`);
-});
